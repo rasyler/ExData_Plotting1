1e4d36a2c9498285058ebc4f3747b7d909a88c64
R. Syler <rhonda.syler@gmail.com>
2014-12-07 21:53:37 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,14 +1,17 @@
-##read in the data from the text file 
-##in the working directory. Read files where date = february 2007
-## Do this by first finding the lines with dates 1/2/2007 through 2/2/2007, save as "proj1.txt"
+##read in the data from the text file
+##Assumption: The text file is saved in the working directory. 
 
+proj1 <- read.table("household_power_consumption.txt", sep=";", header=TRUE, na.strings="?")
 
-proj1 <- read.table("household_power_consumption.txt", nrows = 100, sep=";", header=TRUE, na.strings="?")
-februaryData <- read.table(proj1, ("(^Date)|(^[1|2]/2/2007)",readLines(tmp1), value=TRUE), sep="\n", file="proj1.txt")
+##formating and subseting data -- finding the lines with dates 1/2/2007 through 2/2/2007
+
+proj1$Date<-as.Date(proj1$Date, format="%d/%m/%Y")
+febData<-subset(proj1, Date==as.Date("01/02/2007", format="%d/%m/%Y")| Date==as.Date("02/02/2007", format="%d/%m/%Y"))
+febData$DateTime<-as.POSIXct(paste(febData$Date, febData$Time))
 
 ##create the plot and save as png file
 
-hist(proj1$Global_active_power, col = "red", xlab = "Global Active Power (Kilowatts)", main = "Plot 1")
-par(mfrow = c(1,1), mar = c(5,4,3,3))
+hist(febData$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main = "Global active power")
 dev.copy(png, file="plot1.png")
 dev.off()
+
