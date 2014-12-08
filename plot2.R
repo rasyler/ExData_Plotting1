69654477675dc0ab92ff04faf66f85217ee39fd9
rasyler <rhonda.syler@gmail.com>
2014-12-07 21:56:42 -0600

    Trying to update these files

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-##read in the data from the text file
+##read in the data from the text file 
 ##Assumption: The text file is saved in the working directory. 
 
 proj1 <- read.table("household_power_consumption.txt", sep=";", header=TRUE, na.strings="?")
@@ -9,10 +9,9 @@ proj1$Date<-as.Date(proj1$Date, format="%d/%m/%Y")
 febData<-subset(proj1, Date==as.Date("01/02/2007", format="%d/%m/%Y")| Date==as.Date("02/02/2007", format="%d/%m/%Y"))
 febData$DateTime<-as.POSIXct(paste(febData$Date, febData$Time))
 
-##create the plot and save as png file
+##creating the plot for plot 2 and save as a png file
 
-hist(febData$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main = "Global active power")
-dev.copy(png, file="plot1.png")
+plot(febData$DateTime, febData$Global_active_power, ylab="Global Active Power (kilowatts)", type="l", lwd=1, xlab="")
+dev.copy(png, file="plot2.png")
 dev.off()
-
 
